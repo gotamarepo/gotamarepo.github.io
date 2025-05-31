1d476bacd6355566f5b1fd94765d07b47af79d09
gotamarepo <michael.gotama@outlook.com>
2025-05-31 20:56:18 +0700

    jadi saham

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./lib/**/*.{html,js}","./pages/**/*.{html,js}","./*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
